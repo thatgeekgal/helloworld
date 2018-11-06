d6d9bd728021e98a48a1b5e1562b0be6cee8af0f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-06 08:30:30 +0000

    Deploy thatgeekgal/helloworld to github.com/thatgeekgal/helloworld.git:gh-pages

---
+++ precache-manifest.df85206a9c443d731c82053b414d9c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd8dc0f6b51b5fc2a539",
+    "url": "/static/css/main.6d285973.chunk.css"
+  },
+  {
+    "revision": "cd8dc0f6b51b5fc2a539",
+    "url": "/static/js/main.cd8dc0f6.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fbe7e4ac8bb0333905eefacafb3c044a",
+    "url": "/index.html"
+  }
+];
